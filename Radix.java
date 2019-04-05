c82389369c2c258665a2cde47d66b9717edb7d4a
wyao1 <wyao1@stuy.edu>
2019-04-05 04:12:37 -0400

    added node inside radix sort

--- Radix.java
+++ Radix.java
@@ -19,7 +19,41 @@ public class Radix{
       }
     return maxNum;                     //returns biggest
   } 
-  
+///////////////////////NODE STUFF///////////////////////
+class Node{
+  private Integer data;
+  private Node next,prev;
+  public Node(Integer num, Node nextNum, Node prevNum){
+    data = num;
+    next = nextNum;
+    prev = prevNum;
+  }
+  public Node next(){
+    return next;
+  }
+  public Node prev(){
+    return prev;
+  }
+  public void setNext(Node other){
+    next = other;
+  }
+  public void setPrev(Node other){
+    prev = other;
+  }
+  public Integer getData(){
+    return data;
+  }
+  public Integer setData(Integer i){
+    data = i;
+    return data;            //DOES THIS RETURN OLD DATA!?!?!?!?
+                            //BUMP^^^^
+  }
+  public String toString(){
+    String str = "";
+    return str += data;
+  }
+}
+//////////////////MY LINKED LIST////////////////////
 class MyLinkedList{
   private int size;
   public Node start,end;
